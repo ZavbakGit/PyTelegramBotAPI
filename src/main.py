ad32303ef79084ae58a9f72d148ae747ad001f11
ZavbakGit <ag@799000.ru>
2023-11-22 22:56:51 +1000

    start 1

--- src/main.py
+++ src/main.py
@@ -3,12 +3,40 @@ from private.config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+users = {}
+
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
     chat_id = message.chat.id
     bot.send_message(chat_id,
-                     'Привет! Добро пожаловать в бота сбора обратной связи!')
+                     'Добро пожаловать в бота сбора обратной связи! Введие своё имя')
+    users[chat_id] = {}
+    bot.register_next_step_handler(message, save_username)
+
+
+def save_username(message):
+    chat_id = message.chat.id
+    name = message.text
+    users[chat_id]['name'] = name
+    bot.send_message(chat_id, f'Отлично, {name}. Теперь укажи свою фамилию')
+    bot.register_next_step_handler(message, save_surname)
+
+
+def save_surname(message):
+    chat_id = message.chat.id
+    surname = message.text
+    users[chat_id]['surname'] = surname
+    bot.send_message(chat_id, f'Ваши данные успешно сохранены!')
+
+
+@bot.message_handler(commands=['who_i'])
+def who_i(message):
+    chat_id = message.chat.id
+    name = users[chat_id]['name']
+    surname = users[chat_id]['surname']
+    bot.send_message(chat_id, f'Вы: {name} {surname}')
+
 
 if __name__ == '__main__':
     print('Бот запущен!')
